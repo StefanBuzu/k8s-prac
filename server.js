d539783ec87b94e7e190ce3fc8067abbadd6ecf9
StefanBuzu <stefanbuzu@gmail.com>
2026-07-21 22:25:47 +0200

    E2E flow test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ version: 'v2' }));
+  res.end(JSON.stringify({ version: 'v3' }));
 });
 
 server.listen(3000, () => {
