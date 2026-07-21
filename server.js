c3b9605550cafd4d27e903e58f2662e17eb156e7
StefanBuzu <stefanbuzu@gmail.com>
2026-07-22 01:08:22 +0200

    health probes added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200);
+    return res.end('ok');
+  }
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ version: 'v3' }));
 });
