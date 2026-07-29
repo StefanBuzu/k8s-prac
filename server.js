b55cc1f23a80467261886803f875af0f577b6b82
StefanBuzu <stefanbuzu@gmail.com>
2026-07-29 14:35:11 +0200

    added configmap

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end(JSON.stringify({ version: 'v3' }));
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
